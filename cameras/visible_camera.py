d21835a7b700a3ca8ab292a26c0b1d89eb2071f8
Michał Kułaczkowski <m.kulaczkowski@gmail.com>
2023-07-20 23:57:04 +0200

    updated webstreaming

--- cameras/visible_camera.py
+++ cameras/visible_camera.py
@@ -48,7 +48,7 @@ def thermal_gstreamer_pipeline(
         flip_method=0,
 ):
     return (
-            "4l2src device=/dev/video1 ! "
+            "v4l2src device=/dev/video1 ! "
             "video/x-raw(memory:NVMM), "
             "width=(int)%d, height=(int)%d, "
             "format=(string)NV12, framerate=(fraction)%d/1 ! "
