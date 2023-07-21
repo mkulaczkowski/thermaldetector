a7a5e5ad36ebff8226eeabaebe67a04519d632a5
Michał Kułaczkowski <m.kulaczkowski@gmail.com>
2023-07-21 04:37:58 +0200

    updated webstreaming

--- webstreaming.py
+++ webstreaming.py
@@ -107,7 +107,8 @@ def generate(mode='visible'):
                 cv2.drawContours(outputFrame, contours2, -1, (0, 255, 0), 2, cv2.LINE_AA)
 
             # encode the frame in JPEG format
-
+            if not outputFrame:
+                continue
             (flag, encodedImage) = cv2.imencode(".jpg", outputFrame)
 
         # ensure the frame was successfully encoded
