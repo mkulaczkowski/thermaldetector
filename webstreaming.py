e24779c20bd08627b1aef669313247ddbb0eb3af
Michał Kułaczkowski <m.kulaczkowski@gmail.com>
2023-07-28 01:16:30 +0200

    fusion

--- webstreaming.py
+++ webstreaming.py
@@ -37,9 +37,7 @@ except Exception as e:
 def test_connect():
     print("Connected")
     emit("my response", {"data": "Connected"})
-@socketio.on('json')
-def handle_json(json):
-    print('received json: ' + str(json))
+
 @socketio.on('message')
 def handle_message(data):
     print('received message: ' + data)
