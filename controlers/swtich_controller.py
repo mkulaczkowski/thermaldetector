5ccb6fe225b9c8885bc6039eead69e6cda009321
Michał Kułaczkowski <m.kulaczkowski@gmail.com>
2023-07-28 07:37:27 +0200

    fusion

--- controlers/swtich_controller.py
+++ controlers/swtich_controller.py
@@ -1,18 +1,15 @@
 import RPi.GPIO as GPIO
 import time
 
-switch_camera = 23
-switch_laser = 21
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(switch_camera, GPIO.OUT)
-GPIO.setup(switch_laser, GPIO.OUT)
 
 
 class GPIO_switch():
     switch_camera = 23
     switch_laser = 21
     def __init__(self):
-        pass
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self.switch_camera, GPIO.OUT)
+        GPIO.setup(self.switch_laser, GPIO.OUT)
 
     # BCM pin-numbering scheme from Raspberry Pi
     # set pin as an output pin with optional initial state of HIGH
