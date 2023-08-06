29ca56f32667b7b4f2f942f9fed8e83161b63bcc
Michał Kułaczkowski <m.kulaczkowski@gmail.com>
2023-08-06 21:52:56 +0200

    fusion

--- cameras/opencv_visible_camera.py
+++ cameras/opencv_visible_camera.py
@@ -15,18 +15,32 @@ def visible_gstreamer_pipeline(
             "width=(int)%d, height=(int)%d, "
             "format=(string)NV12, framerate=(fraction)%d/1 ! "
             "nvvidconv flip-method=%d ! "
-            "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
-            "videoconvert ! "
-            "video/x-raw, format=(string)BGR ! appsink drop=1"
+            "omxh264enc ! qtmux ! appsink drop=1"
             % (
                 capture_width,
                 capture_height,
                 framerate,
-                flip_method,
-                display_width,
-                display_height,
+                flip_method
             )
     )
+    # pipeline = (
+    #         "nvarguscamerasrc ! "
+    #         "video/x-raw(memory:NVMM), "
+    #         "width=(int)%d, height=(int)%d, "
+    #         "format=(string)NV12, framerate=(fraction)%d/1 ! "
+    #         "nvvidconv flip-method=%d ! "
+    #         "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+    #         "videoconvert ! "
+    #         "video/x-raw, format=(string)BGR ! appsink drop=1"
+    #         % (
+    #             capture_width,
+    #             capture_height,
+    #             framerate,
+    #             flip_method,
+    #             display_width,
+    #             display_height,
+    #         )
+    # )
     return pipeline
 
 class VisibleCamera(BaseCamera):
