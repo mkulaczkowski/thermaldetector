e7e8d10cbfca95a84a5cf4cdf3e55a0b0bae3c5a
Michał Kułaczkowski <m.kulaczkowski@gmail.com>
2023-08-08 01:34:38 +0200

    fusion

--- controlers/swtich_controller.py
+++ controlers/swtich_controller.py
@@ -24,18 +24,18 @@ class GPIO_switch():
     # set pin as an output pin with optional initial state of HIGH
 
     def thermal_camera_on(self):
-        GPIO.output(self.switch_camera, GPIO.HIGH)
+        GPIO.output(switch_camera, GPIO.HIGH)
 
     def thermal_camera_restart(self):
-        GPIO.output(self.switch_camera, GPIO.LOW)
+        GPIO.output(switch_camera, GPIO.LOW)
         time.sleep(5)
-        GPIO.output(self.switch_camera, GPIO.HIGH)
+        GPIO.output(switch_camera, GPIO.HIGH)
 
     def laser_on(self):
-        GPIO.output(self.switch_laser, GPIO.HIGH)
+        GPIO.output(switch_laser, GPIO.HIGH)
 
     def thermal_camera_off(self):
-        GPIO.output(self.switch_camera, GPIO.LOW)
+        GPIO.output(switch_camera, GPIO.LOW)
 
     def laser_off(self):
         GPIO.output(self.switch_laser, GPIO.LOW)
