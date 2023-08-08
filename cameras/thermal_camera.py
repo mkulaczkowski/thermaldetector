a4514bcf60dd9c78140ceade3791366b97c31311
Michał Kułaczkowski <m.kulaczkowski@gmail.com>
2023-08-08 02:31:36 +0200

    fusion

---
+++ cameras/thermal_camera.py
@@ -0,0 +1,33 @@
+import logging
+import os
+import cv2
+from vidgear.gears import VideoGear
+
+from cameras.opencv_thermal_camera import thermal_gstreamer_pipeline
+
+logger = logging.getLogger('tester.sub')
+
+class ThermalCamera():
+    video_source = thermal_gstreamer_pipeline()
+    thermal_camera = None
+
+    def __init__(self):
+        logger.debug('Thermal init')
+        self.thermal_camera = VideoGear(source=self.video_source, stabilize=False)
+        super(ThermalCamera, self).__init__()
+
+    def __del__(self):
+        logger.debug('VisibleCamera Stop')
+        self.thermal_camera.stop()
+
+    def get_frame(self):
+        while True:
+            # read current frame
+            outputFrame = self.thermal_camera.read()
+
+            # check for stabilized frame if Nonetype
+            if outputFrame is None:
+                break
+            # encode as a jpeg image and return it
+            encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 80]
+            yield cv2.imencode('.jpg', outputFrame, encode_param)[1].tobytes()
