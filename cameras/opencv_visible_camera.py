5d906a9689efdce53c4d04db6dbe3757c21fc087
mkulaczkowski <michal@devopsbay.io>
2024-07-09 03:55:56 +0200

    code optimisations

--- cameras/opencv_visible_camera.py
+++ cameras/opencv_visible_camera.py
@@ -1,76 +1,37 @@
 import os
 import time
-
+from vidgear.gears import CamGear
 import cv2
 import threading
 import logging
-def visible_gstreamer_pipeline(
-        rtsp_url,
-        capture_width=1920,
-        capture_height=1080,
-        framerate=30,
-):
-    # Define the GStreamer pipeline
-    # gst_pipeline = (
-    #     f"rtspsrc location={rtsp_url} latency=0 ! "
-    #     f"rtph264depay ! h264parse ! nvv4l2decoder ! "
-    #     f"video/x-raw(memory:NVMM), width=(int){capture_width}, height=(int){capture_height}, format=(string)NV12 ! "
-    #     f"videoconvert ! video/x-raw, format=(string)BGR ! appsink"
-    # )
 
-    gst_pipeline = (
-        f"rtspsrc location={rtsp_url} ! "
-        "rtph264depay ! "
-        "h264parse ! "
-        "nvv4l2decoder ! "
-        "videoconvert ! "
-        "appsink"
-    )
 
-    gst_pipeline = (f"rtspsrc location={rtsp_url} latency=0 ! "
-        f"rtph264depay ! h264parse ! nvv4l2decoder ! "
-        f"video/x-raw, width=(int){capture_width}, height=(int){capture_height}, format=(string)BGRx ! "
-        f"videoconvert ! video/x-raw, format=(string)BGR ! appsink"
-                    )
-    print(gst_pipeline)
-    return gst_pipeline
+
 
 class OpenCVVisibleCamera:
     def __init__(self, rtsp_url):
         # Initialize video capture only once in the constructor
         logging.info(f'Camera {rtsp_url}')
-        self.capture = cv2.VideoCapture(rtsp_url)
+        self.capture = CamGear(source=rtsp_url, backend=cv2.CAP_FFMPEG).start()
         self.is_running = False
-        self.read_lock = threading.Lock()
         self.frame = None
 
     def start(self):
         if not self.is_running:
             self.is_running = True
-            self.thread = threading.Thread(target=self.update, daemon=True)
-            self.thread.start()
 
     def read(self):
-        with self.read_lock:
-            frame = self.frame.copy() if self.frame is not None else None
-        return frame
+        return self.capture.read()
 
     def stop(self):
         self.is_running = False
-        if self.thread.is_alive():
-            self.thread.join()
 
     def release(self):
-        self.capture.release()
+        self.capture.stop()
 
     def update(self):
         while self.is_running:
-            ret, frame = self.capture.read()
-            with self.read_lock:
-                if ret:
-                    self.frame = frame
-                else:
-                    self.is_running = False
+            self.frame = self.capture.read()
 
     def __del__(self):
         # Release the video capture object when the instance is destroyed
